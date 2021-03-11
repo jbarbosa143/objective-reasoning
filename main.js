37dc4ca3214e9e6d5c9547ecc1791c3c4eae4f98
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-03-11 07:02:00 -0600

    uwu

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
-
-
+function isAdmin(str){
+    if(str.userRole === 'ADMIN'){
+        return true;
+    }else{
+        return false;
+    }
+}
+// ==========================
+function getEmail(email){
+    return((email.firstName.toLowerCase())+ "."+(email.lastName.toLowerCase()) + "@codeimmersives.com")
+}
+// ==================================
+function getPlaylistLength(song){
+    output = song.songs.length;
+    return output;
+}
+// ===================================
+function getHardestHomework(grade){
+    var hardestName = "";
+    var highestPos = 100;
+    for(const score of grade){
+        if(score.averageScore < highestPos){
+            highestPos = score.averageScore;
+            hardestName = score.name;
+        }
+    }
+    return hardestName;
+}
+// ===================================
+function createPhonebook(name,phone){
+    var contactInfo = {};
+    for(i =0; i < name.length; i++){
+        contactInfo[name[i]] = phone[i];
+    }
+    return contactInfo;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
